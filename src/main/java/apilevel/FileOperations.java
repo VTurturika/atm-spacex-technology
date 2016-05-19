ea084b9740ed44d42f264e59e50bcba4f507288b
Victor Turturika <v.turturika@gmail.com>
2016-05-19 23:10:13 +0300

    Added support binary files

---
+++ src/main/java/apilevel/FileOperations.java
@@ -0,0 +1,7 @@
+package apilevel;
+
+/**
+ * Created by Turturika on 19.05.2016.
+ */
+public class FileOperations {
+}
---
+++ src/test/java/apilevel/FileOperationsTest.java
@@ -0,0 +1,26 @@
+package apilevel;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Turturika on 19.05.2016.
+ */
+public class FileOperationsTest {
+    @Test
+    public void createFileForCreditCard() throws Exception {
+
+    }
+
+    @Test
+    public void readCreditCardFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void readServiceKeyFromFile() throws Exception {
+
+    }
+
+}
